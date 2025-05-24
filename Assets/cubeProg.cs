223a89441df0d89710c4f2ddcdf55e20ee36f708
franklinraj <rajfranklin764@gmail.com>
2025-05-24 16:49:38 +0530

    initial comment

--- Assets/cubeProg.cs
+++ Assets/cubeProg.cs
@@ -2,22 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class cubeProg : MonoBehaviour
-{   
+
+public class Coding : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject WinText;
+    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
+
+    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+
     }
-    private void OnMouseDown()
+    private void OnTriggerEnter(Collider other)
     {
-       
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        
+        if (other.gameObject.tag == "Cube")
+        {
+            Destroy(other.gameObject);
+            WinText.SetActive(true);
+        }
     }
 }
